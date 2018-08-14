d268affc9e4b37fee6e236dca9b305c0692798d9
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-08-14 17:58:02 +0800

    Add: user page
    user detail & user post & follow & unfollow

--- app/views.py
+++ app/views.py
@@ -74,6 +74,7 @@ def after_login(email, nickname):
             nickname = email.split('@')[0]
         user = User(nickname=nickname, email=email)
         db.session.add(user)
+        db.session.add(user.follow(user))  # self followed
         db.session.commit()
     remember_me = False
     if "remember_me" in session:
@@ -137,3 +138,43 @@ def internal_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
+
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash(f"User {nickname} is not found.")
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash(f"You can't follow yourself.")
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash(f"Cannot follow {user}.")
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash(f"You are now following {nickname}.")
+    return redirect(url_for('user', nickname=nickname))
+
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash(f"User {nickname} is not found.")
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash(f"You can't unfollow yourself.")
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash(f"Cannot unfollow {user}.")
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash(f"You have stopped following {nickname}.")
+    return redirect(url_for('user', nickname=nickname))
