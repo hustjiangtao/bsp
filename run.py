c41888b3a24eb8eb3bf150131c28f82b9b9fa994
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-08-15 18:42:54 +0800

    Add: Tornado部署

--- run.py
+++ run.py
@@ -4,8 +4,22 @@
 """Run to start server"""
 
 
+# from app import app
+
+# app.run(debug=True)
+# app.run()
+
+import logging
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.options import options
 from app import app
 
 
-# app.run(debug=True)
-app.run()
+if __name__ == '__main__':
+    options.parse_command_line()
+    logging.info('[BSP] bsp is starting...')
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
