dea7b473b9a805ffce658d4dcae4226e49623da7
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-08-17 23:41:03 +0800

    Add: gunicorn

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 # deploy for bsp include nginx conf & supervisord conf
 
 cp /home/jiangtao/bsp/deploy/nginx/bsp.conf /etc/nginx/conf.d/bsp.conf
-cp /home/jiangtao/bsp/deploy/supervisord/bsp.ini /etc/supervisord.d/bsp.ini
+# cp /home/jiangtao/bsp/deploy/supervisord/bsp.ini /etc/supervisord.d/bsp.ini
 nginx -s reload
-supervisorctl update
+# supervisorctl update
+# pipenv run /home/jiangtao/bsp/run.py
+gunicorn -b 127.0.0.1:5000 app:app
