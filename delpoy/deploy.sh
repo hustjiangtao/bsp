daaa4616c0a13e5aac7e301728f3fda0bf61bee5
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-08-17 22:00:59 +0800

    Add: deploy config

---
+++ delpoy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# deploy for bsp include nginx conf & supervisord conf
+
+cp /home/jiangtao/bsp/deploy/nginx/bsp.conf /etc/nginx/conf.d/bsp.conf
+cp /home/jiangtao/bsp/deploy/supervisord/bsp.ini /etc/nginx/conf.d/bsp.ini
+nginx -s reload
+supervisorctl update
