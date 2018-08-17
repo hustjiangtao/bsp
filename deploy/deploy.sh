b989176a19140075266ecbba279cd36b64376e51
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-08-17 22:06:24 +0800

    Update: fix for supervisord conf

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@
 # deploy for bsp include nginx conf & supervisord conf
 
 cp /home/jiangtao/bsp/deploy/nginx/bsp.conf /etc/nginx/conf.d/bsp.conf
-cp /home/jiangtao/bsp/deploy/supervisord/bsp.ini /etc/nginx/conf.d/bsp.ini
+cp /home/jiangtao/bsp/deploy/supervisord/bsp.ini /etc/supervisord.d/bsp.ini
 nginx -s reload
 supervisorctl update
