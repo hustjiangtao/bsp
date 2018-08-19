51d57a2bebc6621ee1e6ff507a54b0bbfa77b46f
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-08-19 09:25:15 +0800

    Update: fix for sqlalchemy conf

--- app/views.py
+++ app/views.py
@@ -102,7 +102,6 @@ def before_request():
     g.user = current_user
     if g.user.is_authenticated:
         g.user.last_seen = datetime.utcnow()
-        db.session.add(g.user)
         db.session.commit()
         g.search_form = SearchForm()
 
