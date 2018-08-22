5ca8b81a59bc6385442a174555a223ff0bee4573
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-08-23 00:15:53 +0800

    Update: bugfix for email blueprint

--- app/emails.py
+++ app/emails.py
@@ -5,9 +5,9 @@
 
 
 from threading import Thread
-from flask import render_template
+from flask import render_template, current_app
 from flask_mail import Message
-from app import app, mail
+from app import mail
 from config import ADMINS
 
 
@@ -29,7 +29,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    send_async_email(app, msg)
+    send_async_email(current_app._get_current_object(), msg)
 
 
 def follower_notification(followed, follower):
