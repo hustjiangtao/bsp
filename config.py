10db542bfdc7805a84b0ef68b8ee31fbc4ef1a2c
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-08-23 11:43:04 +0800

    Add: dotenv for python environ args

--- config.py
+++ config.py
@@ -4,40 +4,31 @@
 """Config for server"""
 
 
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
 # WTF_CSRF_ENABLED = False
 CSRF_ENABLED = True
-SECRET_KEY = "BOOKMARKS_SHARE_PLAN"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-OPENID_PROVIDERS = [
-    {"name": 'Google', "url": 'https://www.google.com/accounts/o8/id'},
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' },
-    { 'name': 'BSP', 'url': 'http://bsp.openid.org.cn/' },
-]
-
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # pagination
 POSTS_PER_PAGE = 3
 
-# whoosh search
-WHOOSH_BASH = os.path.join(basedir, "search.db")
-MAX_SEARCH_RESULTS = 50
-
 # mail
-MAIL_SERVER = "smtp.163.com"
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "test_jiangtao@163.com"
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "h123456"
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
+MAIL_USE_TLS = bool(int(os.environ.get('MAIL_USE_TLS') or 0))  # default False
+MAIL_USE_SSL = bool(int(os.environ.get('MAIL_USE_SSL') or 1))  # default True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 # admins
 ADMINS = ["test_jiangtao@163.com"]
 
