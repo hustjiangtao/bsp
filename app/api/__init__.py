feb65078cfbe3b46a64ce6ea47a34d911ea4160c
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-09-07 20:28:09 +0800

    Add: cross site request support for Ajax

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,9 +5,11 @@
 
 
 from flask import Blueprint
+from flask_cors import CORS
 
 
 bp = Blueprint('api', __name__)
+CORS(bp, supports_credentials=True)  # enable CORS on the API blue print with cookies
 
 
 from app.api import users, errors, tokens
