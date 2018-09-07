060b6ea9e65a5eb9b8e8be6e9ac5644f7d71ea16
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-09-07 21:55:18 +0800

    Add: loginForm by react

--- web/src/components/auth/Login.js
+++ web/src/components/auth/Login.js
@@ -1,17 +1,106 @@
 import React, { Component } from 'react';
 import {Button, Textfield} from 'react-mdl';
 import './Login.css';
+import request from "../../utils/request";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true,
+      account: '',
+      password: '',
+      password_confirm: '',
+    }
+  }
+  handleAccountChange = (e) => {
+    this.setState({account: e.target.value});
+  };
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value});
+  };
+  handlePasswordConfirmChange = (e) => {
+    this.setState({password: e.target.value});
+  };
+  handleLogin = () => {
+    const params = {
+      headers: {
+        "Authorization": 'Basic ' + btoa(this.state.account + ':' + this.state.password)
+      },
+    };
+    request.get('/tokens', params)
+      .then( res => (console.log(res)))
+  };
+  handleRegister = () => {
+    console.log('register...');
+  };
+  handleFormChange = () => {
+    if (document.querySelector(".Login")) {
+      document.querySelector(".Login").classList.add("Register");
+      document.querySelector(".Login").classList.remove("Login");
+    } else {
+      document.querySelector(".Register").classList.add("Login");
+      document.querySelector(".Register").classList.remove("Register");
+    }
+    this.setState({isLoggedIn: !this.state.isLoggedIn});
+  };
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let auth_form = '';
+    if (isLoggedIn) {
+      auth_form = (
+        <form>
+          <Textfield
+            label="Account"
+            floatingLabel
+            name={"account"}
+            value={this.state.account}
+            onChange={this.handleAccountChange}
+          />
+          <Textfield
+            label="Password"
+            floatingLabel
+            name={"password"}
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <Button className="Login-button" raised colored ripple type={"button"} onClick={this.handleLogin}>Login</Button>
+          <Button className="Login-button" raised ripple type={"button"} onClick={this.handleFormChange}>Register</Button>
+        </form>
+      );
+    } else {
+      auth_form = (
+        <form>
+          <Textfield
+            label="Account"
+            floatingLabel
+            name={"account"}
+            value={this.state.account}
+            onChange={this.handleAccountChange}
+          />
+          <Textfield
+            label="Password"
+            floatingLabel
+            name={"password"}
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <Textfield
+            label="Password Confirm"
+            floatingLabel
+            name={"password_confirm"}
+            value={this.state.password_confirm}
+            onChange={this.handlePasswordConfirmChange}
+          />
+          <Button className="Login-button" raised colored ripple type={"button"} onClick={this.handleRegister}>Register</Button>
+          <Button className="Login-button" raised ripple type={"button"} onClick={this.handleFormChange}>Login</Button>
+        </form>
+      );
+    }
     return (
       <div className="Login mdl-card mdl-card__actions">
-        <form>
-          <Textfield label="Username" floatingLabel/>
-          <Textfield label="Password" floatingLabel/>
-          <Button className="Login-button" raised colored ripple>Login</Button>
-          <Button className="Login-button" raised ripple>Register</Button>
-        </form>
+        {auth_form}
       </div>
     );
   }
